3bab166ef62dac83fc597e1dc1e10bec4f771c82
wafuwafu13 <wafuwafu1313@gmail.com>
2020-08-29 17:43:05 +0900

    初期画面

--- web/server.go
+++ web/server.go
@@ -4,7 +4,8 @@ package web
 
 import (
 	"net/http"
-	"io"
+	"html/template"
+	"io/ioutil"
 
 	"github.com/dimfeld/httptreemux"
 	"github.com/justinas/nosurf"
@@ -17,6 +18,34 @@ type Server interface {
 }
 
 const sessionKey = "DIARY_SESSION"
+
+var templates map[string]*template.Template
+
+func init() {
+	var err error
+	templates, err = loadTemplates()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func loadTemplates() (map[string]*template.Template, error) {
+	templates := make(map[string]*template.Template)
+	bs, err := ioutil.ReadAll(Templates.Files["main.tmpl"])
+	if err != nil {
+		return nil, err
+	}
+	mainTmpl := template.Must(template.New("main.tmpl").Parse(string(bs)))
+	for fileName, file := range Templates.Files {
+		bs, err := ioutil.ReadAll(file)
+		if err != nil {
+			return nil, err
+		}
+		mainTmpl := template.Must(mainTmpl.Clone())
+		templates[fileName] = template.Must(mainTmpl.New(fileName).Parse(string(bs)))
+	}
+	return templates, nil
+}
 
 func NewServer(app service.DiaryApp) Server {
 	return &server{app: app}
@@ -47,8 +76,21 @@ var csrfToken = func(r *http.Request) string {
 	return nosurf.Token(r)
 }
 
+func (s *server) renderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, data map[string]interface{}) {
+	if data == nil {
+		data = make(map[string]interface{})
+	}
+	data["CSRFToken"] = csrfToken(r)
+	err := templates[tmpl].ExecuteTemplate(w, "main.tmpl", data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
 func (s *server) indexHandler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Hello, world\n")
+		s.renderTemplate(w, r, "index.tmpl", map[string]interface{}{
+			"Use": "wafuwafu",
+		})
 	})
 }
