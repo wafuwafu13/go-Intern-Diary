14b4c1c8b3ba413a314306f49e34db7b6a524430
wafuwafu13 <wafuwafu1313@gmail.com>
2020-08-30 10:40:18 +0900

    ユーザー登録

--- service/user.go
+++ service/user.go
@@ -1,1 +1,46 @@
 package service
+
+import (
+	"errors"
+	"math/rand"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+	"github.com/hatena/go-Intern-Diary/model"
+)
+
+func (app *diaryApp) CreateNewUser(name string, password string) (err error) {
+	if name == "" {
+		return errors.New("empty user name")
+	}
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	return app.repo.CreateNewUser(name, string(passwordHash))
+}
+
+func (app *diaryApp) FindUserByName(name string) (*model.User, error) {
+	return app.repo.FindUserByName(name)
+}
+
+func generateToken() string {
+	table := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_@"
+	l := len(table)
+	ret := make([]byte, 128)
+	src := make([]byte, 128)
+	rand.Read(src)
+	for i := 0; i < 128; i++ {
+		ret[i] = table[int(src[i])%l]
+	}
+	return string(ret)
+}
+
+func (app *diaryApp) CreateNewToken(userID uint64, expiresAt time.Time) (string, error) {
+	token := generateToken()
+	err := app.repo.CreateNewToken(userID, token, expiresAt)
+	if err != nil {
+		return "", err
+	}
+	return token, nil
+}
